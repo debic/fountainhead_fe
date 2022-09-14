0cc1706c75e3922fb604b5c04afeac81112878f9
IONILz <awiwmaman@outlook.com>
2022-09-14 20:23:15 +0300

    add project, context fix

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { useContext, createContext,useState } from 'react'
+import { useContext, createContext, useState } from 'react'
 import {
-   
+
     useDisclosure,
 
-    
-  } from '@chakra-ui/react';
+
+} from '@chakra-ui/react';
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 export const UserContext = createContext(null);
 
@@ -17,16 +18,14 @@ export function useUserContext() {
 
 
 export default function UserContextProvider({ children }) {
- 
+
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [currentUser, setCurrentUser] = useState('');
     const validate = async () => {
         try {
-
             const res = await axios.get("http://localhost:8080/api/user/validate", { withCredentials: true });
-            setCurrentUser(res.data[0]);
-            localStorage.setItem("user", JSON.stringify(res.data[0]));
-            console.log('dat', res.data[0]);
+            // setCurrentUser(res.data[0]);
+            // localStorage.setItem("user", JSON.stringify(res.data[0]));
             return res.data;
         } catch (error) {
             console.log(error);
@@ -34,45 +33,51 @@ export default function UserContextProvider({ children }) {
     }
 
     const makePayment = async () => {
-        try{
-        const res = await axios.get("http://localhost:8080/api/user/donation", {withCredentials: true});
-        const {url} = res.data
-         window.location.href = url    
-    
-    }catch(err){
+        try {
+            const res = await axios.get("http://localhost:8080/api/user/donation", { withCredentials: true });
+            const { url } = res.data
+            window.location.href = url
+
+        } catch (err) {
             console.log(err)
         }
     }
-    
+
     const gooleAuth = async () => {
-        try{
+        try {
             window.location.href = "http://localhost:8080/api/user/google"
-        // const res = await fetch("http://localhost:8080/api/user/google", {withCredentials: true,
-        // headers:{
-        //     'Access-Control-Allow-Origin': ["http://localhost:3000","http://accounts.google.com"],
-        // }});
-        // axios.get("http://localhost:8080/api/user/google", {withCredentials: true, 
-        // headers: {
-        //     'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET,POST,HEAD', //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept', 'Access-Control-Max-Age': '86400'
-        // }
-    // });
-        // const {url} = res.data
-        //  window.location.href = url    
-    
-    }catch(err){
+
+
+        } catch (err) {
             console.log(err)
         }
     }
-    
+
     const gitHubAuth = async () => {
-        try{
-    
-    window.location.href = "http://localhost:8080/api/user/github"
-        // const res = await axios.get("http://localhost:8080/api/user/github", {withCredentials: true, headers: {'Access-Control-Allow-Origin': ['http://localhost:3000'], 'Access-Control-Allow-Methods': 'GET,POST,HEAD', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept', 'Access-Control-Max-Age': '86400'}});
-        // const {url} = res.data
-        //  window.location.href = url    
-        // console.log(res.data)
-    }catch(err){
+        try {
+
+            window.location.href = "http://localhost:8080/api/user/github"
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const location = useLocation();
+    const splitLocation = (location.pathname).toString().split("/");
+    const projectId = splitLocation[2]
+    const [currentProject, setCurrentProject] = useState([]);
+    const [currentProjectRaitingStudents, setCurrentProjectRaitingStudents] = useState({});
+    const [currentProjectRaitingProfesional, setCurrentProjectRaitingProfesional] = useState({});
+
+    async function getRaitingFunction() {
+        console.log("getRaitingFunction")
+        try {
+            const project = await axios.get(`http://localhost:8080/api/project/vote/${projectId}`, { withCredentials: true })
+            console.log(project)
+            setCurrentProjectRaitingStudents(project.data.studentVotes)
+            setCurrentProjectRaitingProfesional(project.data.clientVotes)
+        } catch (err) {
             console.log(err)
         }
     }
@@ -80,7 +85,7 @@ export default function UserContextProvider({ children }) {
 
     return (
         <div>
-            <UserContext.Provider value={{ validate, setCurrentUser, currentUser, gitHubAuth, gooleAuth, makePayment, setCurrentUser, isOpen, onOpen, onClose }}>
+            <UserContext.Provider value={{ validate, setCurrentUser, currentUser, gitHubAuth, gooleAuth, makePayment, setCurrentUser, isOpen, onOpen, onClose, getRaitingFunction, currentProject, setCurrentProject, currentProjectRaitingStudents, setCurrentProjectRaitingStudents, currentProjectRaitingProfesional, setCurrentProjectRaitingProfesional }}>
                 {children}
             </UserContext.Provider>
         </div>
